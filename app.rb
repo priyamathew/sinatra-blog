fa615232323bf5b5fbd6e56e17b52729adfa9e66
Priya Mathew <pri1229@gmail.com>
2013-06-13 09:16:40 -0500

    initial commit

--- app.rb
+++ app.rb
@@ -6,3 +6,21 @@ require './enviornments'
 
 class Post < ActiveRecord::Base
 end
+
+  get "/" do 
+    @posts = Post.order("created_at DESC")
+    @title = "Welcome."
+    erb :"posts/index"
+  end
+
+
+helpers do 
+  def title
+    if @title
+      "{@title}"
+    else
+      "Welcome."
+    end
+  end
+end
+
